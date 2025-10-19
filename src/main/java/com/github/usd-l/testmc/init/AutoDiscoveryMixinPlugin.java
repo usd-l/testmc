bf694a8e065089487ec79ee3d4cbe2b9632b17a3
GitHub Action <action@github.com>
2025-10-19 18:36:52 +0000

    Initialize template repository proper
    Set package name to com.github.usd-l.testmc
    Set project name to testmc
    Set modid to testmc

--- src/main/java/com/github/usd-l/testmc/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/usd-l/testmc/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.usd-l.testmc.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/usd-l/testmc/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/usd-l/testmc/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.usd-l.testmc.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
